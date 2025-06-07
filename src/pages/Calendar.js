31c5c1b947aa3e7ceaaf4cc56327e9bd674ee4ef
Anna <agargalloso@campus.eug.es>
2025-06-07 13:21:21 +0200

    Pop-up de calendario

--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,11 +1,23 @@
 //React & MUI imports:
-import { useState, useEffect } from 'react';
-import { Box, Typography, } from "@mui/material";
+import React, { useState, useEffect } from 'react';
+import { Box, Button, Divider, Grid, Modal, Typography, } from "@mui/material";
 
 //Calendar imports:
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
+
+//Pop-up style:
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 500,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function Appointments() {
   //Localizer for the calendar:
@@ -14,6 +26,9 @@ export default function Appointments() {
 
   //Data obtained from the API:
   const [data, setData] = useState([]);
+
+  //Trigger for the more info pop-up:
+  const [open, setOpen] = React.useState(false);
 
   //Do this when the page load:
   useEffect(() => {
@@ -32,7 +47,10 @@ export default function Appointments() {
       const formatedData = jsonData.map((item) => {
         const startDate = new Date(item.date);
         return {
-          title: item.type + "\nMascota: " + item.pet_id + "\nVet: " + item.user_id,
+          title: item.type,
+          description: item.description,
+          pet: item.pet_id,
+          vet: item.user_id,
           start: new Date(item.date),
           end: new Date(startDate.getTime() + 2 * 60 * 60 * 1000)
         };
@@ -41,6 +59,11 @@ export default function Appointments() {
     } catch (error) {
       console.error('Error al obtener datos de la API:', error);
     }
+  }
+
+  //When an appointment is clicked, do:
+  function handleSelectEvent(event) {
+    setOpen({ open: true, event: event });
   }
 
   return (
@@ -63,8 +86,40 @@ export default function Appointments() {
             week: "Semana",
             day: "Día"
           }}
+          onSelectEvent={handleSelectEvent}
         />
       </div>
+
+      {/*Pop-up*/}
+      <Modal open={open.open} onClose={() => setOpen({ open: false })}>
+        <Box sx={style}>
+          {/*Title*/}
+          <Typography variant="h6" >{open.event?.title}</Typography>
+          <Typography variant='body' >{open.event?.description}</Typography>
+          <Divider sx={{ marginBottom: "10px" }} />
+          <Grid container >
+            <Grid item size={12}>
+            </Grid>
+            <Grid item size={12}>
+              <Typography variant='body' sx={{ fontWeight: 'bold' }}>Mascota: </Typography>
+              <Typography variant='body'>{open.event?.pet}</Typography>
+            </Grid>
+            <Grid item size={12}>
+              <Typography variant='body' sx={{ fontWeight: 'bold' }}>Veterniario asignado: </Typography>
+              <Typography variant='body'>{open.event?.vet}</Typography>
+            </Grid>
+          </Grid>
+          <Box sx={{ mt: 2, display: "flex", justifyContent: "flex-end" }}>
+            <Grid container spacing={2}>
+              <Grid item>
+                <Button onClick={() => setOpen({ open: false })} variant="contained" color="error">
+                  Cerrar
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Modal>
     </Box>
   );
 
